08b7fc9d0971777a9998f3388b3c4eea6ae6ddd7
Chris Ware <c.p.ware@hotmail.com>
2025-02-06 00:13:37 -0700

    Nav + logo update

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,5 +1,6 @@
 import { getMenu } from "@/lib/shopify";
 import styles from "./Nav.module.css";
+import Logo from "../Logo/Logo";
 
 export async function Nav() {
   const menu = await getMenu("next-js-frontend-menu");
@@ -7,7 +8,12 @@ export async function Nav() {
 
   return (
     <header className={styles.container}>
-      <nav>nav here</nav>
+      <nav className={styles.nav}>
+        <div className={styles.left}>
+          <Logo />
+        </div>
+        <div className={styles.right}>Nav Item + Cart here</div>
+      </nav>
     </header>
   );
 }
