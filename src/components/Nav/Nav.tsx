a80f5f15a0a4b020708061dd1727f0897a742e26
Chris Ware <c.p.ware@hotmail.com>
2025-02-06 08:38:11 -0700

    svg webpack + nav update

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,6 +1,7 @@
 import { getMenu } from "@/lib/shopify";
 import styles from "./Nav.module.css";
 import Logo from "../Logo/Logo";
+import Cart from "../../../public/icons/shoppingCart.svg";
 
 export async function Nav() {
   const menu = await getMenu("next-js-frontend-menu");
@@ -12,7 +13,11 @@ export async function Nav() {
         <div className={styles.left}>
           <Logo />
         </div>
-        <div className={styles.right}>Nav Item + Cart here</div>
+        <div className={styles.right}>
+          <div className={styles.iconContainer}>
+            <Cart className={styles.icon} />
+          </div>
+        </div>
       </nav>
     </header>
   );
