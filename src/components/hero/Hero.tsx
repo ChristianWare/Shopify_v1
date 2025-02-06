107fc1480d1ef5999e46483a08f66fa0015eb133
Chris Ware <c.p.ware@hotmail.com>
2025-02-06 01:51:57 -0700

    Hero done

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,14 +1,15 @@
 import LayoutWrapper from "../LayoutWrapper";
 import styles from "./Hero.module.css";
-import Image from "next/image";
-import Img1 from "../../../public/images/hero.jpg";
+// import Image from "next/image";
+// import Img1 from "../../../public/images/hero.jpg";
+import ProductPreview from "../ProductPreview/ProductPreview";
 
 export default function Hero() {
   return (
     <section className={styles.container}>
       <LayoutWrapper>
         <div className={styles.content}>
-          <Image src={Img1} alt='' title='' fill className={styles.img} />
+          {/* <Image src={Img1} alt='' title='' fill className={styles.img} /> */}
           <div className={styles.imgOverlay}></div>
           <div className={styles.contentChildren}>
             <div className={styles.left}>
@@ -19,10 +20,13 @@ export default function Hero() {
                 Your shop for bicycles <br /> and e-bikes in SWitzerland.
               </p>
             </div>
-            <div className={styles.right}>right</div>
+            <div className={styles.right}>
+              <ProductPreview />
+            </div>
           </div>
         </div>
       </LayoutWrapper>
     </section>
   );
 }
+
