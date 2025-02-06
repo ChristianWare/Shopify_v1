7b60a2330fcce02489c0c2c832ef95d79c619679
Chris Ware <c.p.ware@hotmail.com>
2025-02-06 00:06:21 -0700

    Barlo condensed font added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,28 @@
 import type { Metadata } from "next";
-import { Inter_Tight } from "next/font/google";
+import {
+  Inter_Tight,
+  Barlow_Condensed,
+  Barlow_Semi_Condensed,
+} from "next/font/google";
 import "./globals.css";
 import { Nav } from "@/components/Nav/Nav";
 
 const interTight = Inter_Tight({
   variable: "--interTight",
   subsets: ["latin"],
+  weight: ["400", "500", "600"],
+});
+
+const barlowCondensed = Barlow_Condensed({
+  variable: "--barlowCondensed",
+  subsets: ["latin"],
+  weight: ["100", "700", "800"],
+});
+
+const barlowSemiCondensed = Barlow_Semi_Condensed({
+  variable: "--barlowSemiCondensed",
+  subsets: ["latin"],
+  weight: ["700"],
 });
 
 export const metadata: Metadata = {
@@ -20,7 +37,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${interTight.variable}`}>
+      <body
+        className={`${interTight.variable} ${barlowCondensed.variable} ${barlowSemiCondensed.variable}`}
+      >
         <Nav />
         {children}
       </body>
